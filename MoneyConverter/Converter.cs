dee90c6724a39fa76917f2de28d3759f69035204
TretiakTS <114629615+TretiakTS@users.noreply.github.com>
2022-09-29 04:08:45 +0300

    Add files via upload

---
+++ MoneyConverter/Converter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyConvertation
+{
+    internal class Converter
+    {
+        private readonly decimal dollarRate;
+        private readonly decimal euroRate;
+        public Converter(decimal dollar, decimal euro)
+        {
+            this.dollarRate = dollar;
+            this.euroRate = euro;
+        }
+        public double ConvertHryvniaToDollar(double hryvny)
+            => hryvny / Convert.ToDouble(dollarRate);
+
+        public double ConvertHryvniaToEuro(double hryvny)
+            => hryvny / Convert.ToDouble(euroRate);
+        public double ConvertDollarToHryvnia(double dollar)
+            => Convert.ToDouble(dollarRate) * dollar;
+        public double ConvertEuroToHryvnia(double euro)
+            => Convert.ToDouble(euroRate) * euro;
+    }
+}
---
+++ MoneyConverter/Program.cs
@@ -0,0 +1,29 @@
+﻿using MoneyConvertation;
+Console.WriteLine("Enter amount of hryvnia");
+double hryvnia = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the dollar Rate");
+Decimal dollarRate = Decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the euro Rate");
+Decimal euroRate = Decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Write the amount of dollars");
+double dollar = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Write the amount of euros");
+Double euro = Double.Parse(Console.ReadLine());
+
+Converter converter = new Converter(dollarRate, euroRate);
+
+string result = string.Format("{0:f2}", converter.ConvertHryvniaToDollar(hryvnia));
+Console.WriteLine("Hryvnia to dollar " + result);
+
+result = string.Format("{0:f2}", converter.ConvertHryvniaToEuro(hryvnia));
+Console.WriteLine("Hryvnia to euro " + result);
+
+result = string.Format("{0:f2}", converter.ConvertDollarToHryvnia(dollar));
+Console.WriteLine("Dollar to hryvnia" + result);
+
+result = string.Format("{0:f2}", converter.ConvertEuroToHryvnia(euro));
+Console.WriteLine("Euro to hryvnia" + result);
